8df879a803e9b665054037e1f654a20d795ec4cb
Feret Thomas <thomas.feret@hotmail.fr>
2026-05-12 16:07:43 +0200

    init : configuration de la stack complète

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import glsl from 'vite-plugin-glsl'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    glsl(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
+  },
+})
